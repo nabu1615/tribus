3edbf8a5c6e58cd0341ea808cc71f8756bee0ded
Javier Vargas <jvargas@hugeinc.com>
2018-10-20 16:12:29 -0500

    scroll animation behaviors

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,46 @@
 import '../scss/main.scss'
 
-console.log('hello');
+
+let controller = new ScrollMagic.Controller(),
+    horizontalSlide = new TimelineMax()
+    // animate panels
+    .to("#js-slideContainer", 1, { x: "-20%" })
+    .to("#js-slideContainer", 1, { x: "-40%" })
+    .to("#js-slideContainer", 1, { x: "-60%" })
+    .to("#js-slideContainer", 1, { x: "-80%" })
+
+document.getElementById('about').addEventListener('click', (e) => {
+    e.preventDefault();
+    new TimelineMax().to("#js-slideContainer", 1, { x: "-20%" });
+});
+
+document.getElementById('services').addEventListener('click', (e) => {
+    e.preventDefault();
+    new TimelineMax().to("#js-slideContainer", 1, { x: "-40%" });
+});
+
+document.getElementById('team').addEventListener('click', (e) => {
+    e.preventDefault();
+    new TimelineMax().to("#js-slideContainer", 1, { x: "-60%" });
+});
+
+document.getElementById('contact').addEventListener('click', (e) => {
+    e.preventDefault();
+    new TimelineMax().to("#js-slideContainer", 1, { x: "-80%" });
+});
+
+document.getElementById('logo').addEventListener('click', (e) => {
+    e.preventDefault();
+    new TimelineMax().to("#js-slideContainer", 1, { x: "0%" });
+});
+
+// create scene to pin and link animation
+new ScrollMagic.Scene({
+    triggerElement: "#js-wrapper",
+    triggerHook: "onLeave",
+    duration: "400%"
+})
+    .setPin("#js-wrapper")
+    .setTween(horizontalSlide)
+    .addTo(controller);
+    //.addIndicators() // add indicators (requires plugin)
